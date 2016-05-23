50db3c67a6c5b300c3e4d2b47bc351bebddce14c
Patrick Schnell <schnellp@gmail.com>
2016-05-23 16:48:39 -0500

    Added "add food" activity

--- app/src/main/java/net/schnellp/mycapnutrition/SelectFood.java
+++ app/src/main/java/net/schnellp/mycapnutrition/SelectFood.java
@@ -1,6 +1,7 @@
 package net.schnellp.mycapnutrition;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -60,12 +61,12 @@ public class SelectFood extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_food_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), AddFood.class);
+                startActivity(intent);
             }
         });
     }
