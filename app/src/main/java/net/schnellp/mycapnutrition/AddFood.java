6e47494999c72a9f01920d73fdc1cf8336629f0d
Patrick Schnell <schnellp@gmail.com>
2016-05-24 17:56:01 -0500

    Automatically show keyboard on AddFood activity start

--- app/src/main/java/net/schnellp/mycapnutrition/AddFood.java
+++ app/src/main/java/net/schnellp/mycapnutrition/AddFood.java
@@ -1,5 +1,6 @@
 package net.schnellp.mycapnutrition;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
