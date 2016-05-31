b3a772b31d29571094951fe6a475c600a4a9c5e9
Patrick Schnell <schnellp@gmail.com>
2016-05-31 16:28:52 -0500

    Created placeholder Settings activity

--- app/src/main/java/net/schnellp/mycapnutrition/View/JournalDayView.java
+++ app/src/main/java/net/schnellp/mycapnutrition/View/JournalDayView.java
@@ -91,6 +91,8 @@ public class JournalDayView extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
             return true;
         }
 
