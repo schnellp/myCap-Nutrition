222aa118693befcdd44fa53acd4992bfe820f247
Patrick Schnell <schnellp@gmail.com>
2016-06-07 15:59:32 -0500

    Removed old context menu code

--- app/src/main/java/net/schnellp/mycapnutrition/View/JournalDayFragment.java
+++ app/src/main/java/net/schnellp/mycapnutrition/View/JournalDayFragment.java
@@ -2,23 +2,16 @@ package net.schnellp.mycapnutrition.View;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ExpandableListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import net.schnellp.mycapnutrition.Model.IntOrNA;
 import net.schnellp.mycapnutrition.Model.ObjectMath;
-import net.schnellp.mycapnutrition.Model.Record;
 import net.schnellp.mycapnutrition.Presenter.ExpandableRecordListAdapter;
 import net.schnellp.mycapnutrition.R;
 
@@ -28,7 +21,6 @@ public class JournalDayFragment extends Fragment {
     public static final String DATE = "day_number";
     public String date;
     public ExpandableRecordListAdapter adapter;
-    Record tempRecord;
     View rootView;
 
     @Override
@@ -45,7 +37,6 @@ public class JournalDayFragment extends Fragment {
         listView.setAdapter(adapter);
         listView.setOnGroupClickListener(adapter);
         listView.setOnItemLongClickListener(adapter);
-        //registerForContextMenu(listView);
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
         int goalKcal = Integer.parseInt(sharedPref.getString("goal_kcal", "2000"));
@@ -57,57 +48,5 @@ public class JournalDayFragment extends Fragment {
         progressText.setText(kcal + " / " + goalKcal + " kcal");
 
         return rootView;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        System.out.println("created from " + date);
-        super.onCreateContextMenu(menu, v, menuInfo);
-        if (v.getId()==R.id.recordListView) {
-            MenuInflater inflater = this.getActivity().getMenuInflater();
-            inflater.inflate(R.menu.menu_context_generic_delete, menu);
-        }
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        if (getUserVisibleHint()) {
-            ExpandableListView.ExpandableListContextMenuInfo info = (ExpandableListView.ExpandableListContextMenuInfo) item.getMenuInfo();
-            switch(item.getItemId()) {
-                case R.id.delete:
-                    Snackbar snackbar = Snackbar
-                            .make(getActivity().findViewById(R.id.main_content), "Record deleted.",
-                                    Snackbar.LENGTH_LONG)
-                            .setAction("UNDO", new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    Snackbar snackbar1 = Snackbar.make(getActivity()
-                                                    .findViewById(R.id.main_content),
-                                            "Food is restored!", Snackbar.LENGTH_SHORT);
-                                    JournalDayFragment.this.adapter.restoreItem(tempRecord);
-                                    snackbar1.show();
-                                }
-                            });
-                    snackbar.show();
-                    tempRecord = (Record) adapter.getGroup(
-                            ExpandableListView.getPackedPositionGroup(info.packedPosition));
-                    deleteRecord(ExpandableListView.getPackedPositionGroup(info.packedPosition));
-                    return true;
-                default:
-                    return super.onContextItemSelected(item);
-            }
-        } else {
-            return false;
-        }
-
-    }
-
-    public void deleteRecord(int position) {
-        adapter.deleteItem(position);
     }
 }
